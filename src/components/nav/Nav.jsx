01762e1e50b9bf43b4cab30f041bb4428f6c430f
Vital <balutavili@gmail.com>
2022-12-26 03:42:24 +0200

    manage nav component

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,7 +1,56 @@
+import { useState } from "react";
+import {
+  SlHome,
+  SlUser,
+  SlNotebook,
+  SlBriefcase,
+  SlSpeech,
+} from "react-icons/sl";
+
 import "./nav.css";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  const [activeNav, setActiveNav] = useState("#");
+
+  return (
+    <nav>
+      <a
+        href="#"
+        className={activeNav === "#" ? "active" : ""}
+        onClick={() => setActiveNav("#")}
+      >
+        <SlHome />
+      </a>
+      <a
+        href="#about"
+        className={activeNav === "#about" ? "active" : ""}
+        onClick={() => setActiveNav("#about")}
+      >
+        <SlUser />
+      </a>
+      <a
+        href="#experience"
+        className={activeNav === "#experience" ? "active" : ""}
+        onClick={() => setActiveNav("#experience")}
+      >
+        <SlNotebook />
+      </a>
+      <a
+        href="#portfolio"
+        className={activeNav === "#portfolio" ? "active" : ""}
+        onClick={() => setActiveNav("#portfolio")}
+      >
+        <SlBriefcase />
+      </a>
+      <a
+        href="#contact"
+        className={activeNav === "#contact" ? "active" : ""}
+        onClick={() => setActiveNav("#contact")}
+      >
+        <SlSpeech />
+      </a>
+    </nav>
+  );
 };
 
 export default Nav;
