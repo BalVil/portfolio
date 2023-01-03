505ec6ccb7a44fcb2a8a2df7f1529f73dbb92271
Vital <balutavili@gmail.com>
2023-01-03 23:30:15 +0200

    added footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,13 +1,14 @@
-import { FaFacebookF } from "react-icons/fa";
-import { IoLogoTwitter } from "react-icons/io";
 import "./footer.css";
 
 const Footer = () => {
   return (
     <footer>
-      <a href="#" className="footer__logo">
-        BALUTA
-      </a>
+      <div>
+        <a href="#" className="footer__logo">
+          BALUTA
+        </a>
+      </div>
+
       <ul className="permalinks">
         <li href="#top">Home</li>
         <li href="#about">About</li>
@@ -16,14 +17,6 @@ const Footer = () => {
         <li href="#testimonials">Testimonials</li>
         <li href="#contact">Contact</li>
       </ul>
-      <div className="footer__socials">
-        <a href="https://facebook">
-          <FaFacebookF />
-        </a>
-        <a href="https://twitter">
-          <IoLogoTwitter />
-        </a>
-      </div>
       <div className="footer__copyright">
         <small>&copy; Baluta Portfolio. All rights reserved.</small>
       </div>
